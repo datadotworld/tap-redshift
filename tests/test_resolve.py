a915c37c4b5d1c4aaa16d2acc629426b5bdf6d68
andela-gokerayi <gbolahan.okerayi@andela.com>
2018-01-10 19:34:22 +0100

    Add more unit test

--- tests/test_resolve.py
+++ tests/test_resolve.py
@@ -1,9 +1,47 @@
 import tap_redshift
+import mock
+import singer
 
+import pytest
 from doublex import assert_that
-from hamcrest import equal_to
+from hamcrest import equal_to, calling, raises, has_key, instance_of
 
 from singer.schema import Schema
+
+
+@pytest.fixture()
+def db_config():
+    config = {
+        'host': 'host',
+        'port': '',
+        'dbname': 'FakeDB',
+        'user': 'user',
+        'password': 'password'
+    }
+    return config
+
+
+expected_catalog = {
+    'streams': [{
+        'database_name': 'FakeDB',
+        'schema': {
+            'type': 'object',
+            'selected': "true",
+            'properties': {
+                'id': {
+                    "selected": "true",
+                    'minimum': -2147483648,
+                    'type': 'integer',
+                    'maximum': 2147483647,
+                    'inclusion': 'available'
+                }
+            },
+            'table_name': 'fake name',
+            'stream': 'fake stream',
+            'tap_stream_id': 'FakeDB-fake name'
+        }
+    }]
+}
 
 
 class TestResolve(object):
@@ -17,3 +55,24 @@ class TestResolve(object):
         selected_col = tap_redshift.resolve.desired_columns(desired_cols,
                                                             table_schema)
         assert_that(selected_col, equal_to(set(['col1'])))
+
+    def test_unknown_inclusion(self):
+        selected_cols = set(['col1'])
+        table_schema = Schema(type='object',
+                              properties={
+                                  'col1': Schema(None, inclusion='unknown'),
+                                  'col2': Schema(None, inclusion='unsupported')
+                              })
+        assert_that(calling(
+            tap_redshift.resolve.desired_columns).with_args(
+                selected_cols, table_schema), raises(Exception))
+
+    @mock.patch("psycopg2.connect")
+    def test_resolve_catalog(self, mock_connect, db_config):
+        mock_con = mock_connect.return_value
+        mock_cur = mock_con.cursor.return_value
+        catalog = tap_redshift.discover_catalog(mock=db_config)
+        state = tap_redshift.build_state({}, catalog)
+        resolved_cat = tap_redshift.resolve.resolve_catalog(catalog, state)
+        assert_that(resolved_cat, instance_of(singer.catalog.Catalog))
+        assert_that(resolved_cat.to_dict(), has_key(equal_to('streams')))
--- tests/test_tap_redshift.py
+++ tests/test_tap_redshift.py
@@ -1,9 +1,10 @@
 import tap_redshift
 import mock
+import singer
 
 import pytest
-from doublex import assert_that
-from hamcrest import has_key, equal_to
+from doublex import assert_that, called
+from hamcrest import has_key, equal_to, instance_of
 
 
 sample_db_data = {
@@ -48,58 +49,116 @@ sample_db_data = {
     ]
 }
 
+
 expected_result = {
-    'streams': [{
-        'database_name': 'FakeDB',
-        'schema': {
-            'type': 'object',
-            'properties': {
-                'id': {
-                    'minimum': -2147483648,
-                    'type': 'integer',
-                    'maximum': 2147483647,
-                    'inclusion': 'available'
+    "streams": [
+        {
+            "database_name": "FakeDB",
+            "is_view": False,
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "id": {
+                        "minimum": -2147483648,
+                        "type": "integer",
+                        "maximum": 2147483647,
+                        "inclusion": "available"
+                    },
+                    "is_bool": {
+                        "type": [
+                            "null",
+                            "boolean"
+                        ],
+                        "inclusion": "available"
+                    },
+                    "float": {
+                        "type": [
+                            "null",
+                            "number"
+                        ],
+                        "inclusion": "available"
+                    },
+                    "decimal": {
+                        "type": [
+                            "null",
+                            "number"
+                        ],
+                        "exclusiveMaximum": True,
+                        "inclusion": "available"
+                    },
+                    "varchar": {
+                        "type": "string",
+                        "inclusion": "available"
+                    },
+                    "expires_at": {
+                        "type": [
+                            "null",
+                            "string"
+                        ],
+                        "format": "date-time",
+                        "inclusion": "available"
+                    }
                 },
-                'is_bool': {
-                    'type': [
-                        'null',
-                        'boolean'
-                    ],
-                    'inclusion': 'available'
-                },
-                'float': {
-                    'type': [
-                        'null',
-                        'number'
-                    ],
-                    'inclusion': 'available'
-                },
-                'decimal': {
-                    'type': [
-                        'null',
-                        'number'
-                    ],
-                    'exclusiveMaximum': True,
-                    'inclusion': 'available'
-                },
-                'varchar': {
-                    'type': 'string',
-                    'inclusion': 'available'
-                },
-                "expires_at": {
-                    "type": [
-                        "null",
-                        "string"
-                    ],
-                    'format': 'date-time',
-                    'inclusion': 'available'
-                }
             },
-            'table_name': 'fake name',
-            'stream': 'fake stream',
-            'tap_stream_id': 'FakeDB-fake name'
+            "metadata": [
+                {
+                    "metadata": {
+                        "sql-datatype": "int4",
+                        "selected-by-default": True
+                    },
+                    "breadcrumb": ["properties", "id"]
+                },
+                {
+                    "metadata": {
+                        "sql-datatype": "bool",
+                        "selected-by-default": True
+                    },
+                    "breadcrumb": ["properties", "is_bool" ]
+                },
+                {
+                    "metadata": {
+                        "sql-datatype": "int4",
+                        "selected-by-default": True
+                    },
+                    "breadcrumb": ["properties", "float" ]
+                },
+                {
+                    "metadata": {
+                        "sql-datatype": "int4",
+                        "selected-by-default": True
+                    },
+                    "breadcrumb": ["properties", "decimal" ]
+                },
+                {
+                    "metadata": {
+                        "sql-datatype": "varchar",
+                        "selected-by-default": True
+                    },
+                    "breadcrumb": ["properties", "varchar"]
+                },
+                {
+                    "metadata": {
+                        "sql-datatype": "timestamptz",
+                        "selected-by-default": True
+                    },
+                    "breadcrumb": ["properties", "expires_at"]
+                },
+            ],
+            "key_properties": [
+                "id",
+                "is_bool",
+                "float",
+                "decimal",
+                "varchar",
+                "expires_at"
+            ],
+            "is_view": False,
+            "table_name": "fake name",
+            "stream": "fake stream",
+            "tap_stream_id": "FakeDB-fake name"
         }
-    }]}
+    ]
+}
 
 
 @pytest.fixture()
@@ -114,6 +173,15 @@ def db_config():
     return config
 
 
+def message_types_and_versions(messages):
+    message_types = []
+    for message in messages:
+        t = type(message)
+        if t in set([singer.StateMessage, singer.SchemaMessage]):
+            message_types.append(t.__name__)
+    return message_types
+
+
 class TestRedShiftTap(object):
     @mock.patch("psycopg2.connect")
     def test_discover_catalog(self, mock_connect, db_config):
@@ -122,8 +190,16 @@ class TestRedShiftTap(object):
         mock_cur.fetchall.return_value = expected_result
         result = tap_redshift.discover_catalog(mock=db_config).to_dict()
         assert_that(result, has_key(equal_to('streams')))
-        streams = result['streams'][0]
-        assert_that(streams, has_key(equal_to('schema')))
+        result_schema = result['streams'][0]
+        assert_that(result_schema, has_key(equal_to('schema')))
+
+    @mock.patch("psycopg2.connect")
+    def test_unsupported_col(self, mock_connect, db_config):
+        mock_con = mock_connect.return_value
+        mock_cur = mock_con.cursor.return_value
+        mock_cur.fetchall.return_value = expected_result
+        result = tap_redshift.discover_catalog(mock=db_config)
+        assert_that(result.streams[0].key_properties, equal_to(None))
 
     def test_type_int4(self):
         col = sample_db_data['columns'][0]
@@ -166,3 +242,36 @@ class TestRedShiftTap(object):
         stream_schema = expected_result['streams'][0]
         expected_schema = stream_schema['schema']['properties']['expires_at']
         assert_that(column_schema, equal_to(expected_schema))
+
+    @mock.patch("psycopg2.connect")
+    def test_no_col_selected(self, mock_connect, db_config):
+        mock_con = mock_connect.return_value
+        mock_cur = mock_con.cursor.return_value
+        mock_cur.fetchall.return_value = expected_result
+        catalog = tap_redshift.discover_catalog(mock=db_config)
+        state = tap_redshift.build_state({}, catalog)
+        message_types = message_types_and_versions(
+            tap_redshift.generate_messages(catalog, state, mock=db_config))
+        assert_that((message_types, equal_to(['StateMessage'])))
+
+    @mock.patch("psycopg2.connect")
+    def test_build_state(self, mock_connect, db_config):
+        mock_con = mock_connect.return_value
+        mock_cur = mock_con.cursor.return_value
+        mock_cur.fetchall.return_value = expected_result
+        catalog = tap_redshift.discover_catalog(mock=db_config)
+        for stream in catalog.streams:
+            stream.replication_key = 'id'
+        state = tap_redshift.build_state({}, catalog)
+        assert_that(state, has_key(equal_to('bookmarks')))
+
+    @mock.patch("psycopg2.connect")
+    def test_stream_version(self, mock_connect, db_config):
+        mock_con = mock_connect.return_value
+        mock_cur = mock_con.cursor.return_value
+        mock_cur.fetchall.return_value = expected_result
+        catalog = tap_redshift.discover_catalog(mock=db_config)
+        stream_id = catalog.to_dict()['streams'][0]['tap_stream_id']
+        state = tap_redshift.build_state({}, catalog)
+        stream_version = tap_redshift.get_stream_version(stream_id, state)
+        assert_that(stream_version, instance_of(int))
