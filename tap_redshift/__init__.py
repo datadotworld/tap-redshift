e112b276c8955d4097f631e5da8a2cb072e9d064
Dashiel Lopez Mendez <lopez.mendez.d@gmail.com>
2018-05-22 23:01:28 -0500

    Enclosing column names in quotes
    Fix for #18. Some light reading: https://docs.aws.amazon.com/redshift/latest/dg/r_names.html

--- tap_redshift/__init__.py
+++ tap_redshift/__init__.py
@@ -286,7 +286,7 @@ def sync_table(connection, catalog_entry, state):
     tap_stream_id = catalog_entry.tap_stream_id
     LOGGER.info('Beginning sync for {} table'.format(tap_stream_id))
     with connection.cursor() as cursor:
-        columns = [c for c in columns]
+        columns = ['"{}"'.format(c) for c in columns]
         select = 'SELECT {} FROM {}'.format(
             ','.join(columns),
             catalog_entry.table)
